00f3725ad6f0ceb6c1f1427bbb764de2b7293b73
daemon-cool <damian.damian.tofik@gmail.com>
2020-03-18 13:27:52 +0100

    Constant diff is in milliseconds, so added calculation for readable values days, hours, minuts and seconds

--- script.js
+++ script.js
@@ -11,9 +11,12 @@ const newYearTime = new Date(`January 01 ${currentYear + 1} 00:00:00`);
 function updateCountDown() {
     const currentTime = new Date();
     const diff = newYearTime - currentTime;
-    console.log(currentTime);
-    console.log(newYearTime);
-    console.log(diff);
+
+    const d = Math.floor(diff / 1000 / 60 / 60 / 24);
+    const h = Math.floor(diff / 1000 / 60 / 60);
+    const m = Math.floor(diff / 1000 / 60);
+    const s = Math.floor(diff / 1000);
+    console.log(seconds);
 }
 
 updateCountDown();
